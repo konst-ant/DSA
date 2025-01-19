778a2bd9eb927caba481a80ae277318ce22076d7
Konstantin Antipin <geklbery@gmail.com>
2025-01-19 11:20:47 +1100

    unlimited DP: MinimumCoinChangeTabular add better implementation

--- src/main/java/dp/unlimited/MinimumCoinChangeTabular2.java
+++ src/main/java/dp/unlimited/MinimumCoinChangeTabular2.java
@@ -17,16 +17,12 @@ package dp.unlimited;
  * Result: 4
  * Min set of two coins {2,3,3,3} will make 11
  */
-public class MinimumCoinChangeTabular {
+
+// Note: this class is kinda "weird" but working implementation >< (see MinimumCoinChangeTabular for a more clear way)
+public class MinimumCoinChangeTabular2 {
 
     public int minimumNumberOfCoins(int[] coins, int sum) {
         int[][] dp = new int[coins.length][sum + 1];
-
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j < sum + 1; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-            }
-        }
 
         // for sum = 0 we need 0 coins with any coin set
         for (int i = 0; i < coins.length; i++) {
@@ -36,20 +32,21 @@ public class MinimumCoinChangeTabular {
         for (int i = 0; i < coins.length; i++) {
             for (int j = 1; j < sum + 1; j++) {
                 // first case: exclude coin i
+                int minCandidate1 = Integer.MAX_VALUE;
                 if (i > 0) {
-                    dp[i][j] = dp[i - 1][j];
+                    minCandidate1 = dp[i - 1][j];
                 }
 
                 // second case: include coin i
-                // Second condition is being safely checked, i.e. only when j - coins[i] >= 0
-                //
-                // Note also, second condition assures that the "step back" cell is the legitimate one that is was matched before
-                // To understand it better, set breakpoint on return line and see dp matrix result, starting from it's 1-st line
-                if (coins[i] <= j && dp[i][j - coins[i]] != Integer.MAX_VALUE) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][j - coins[i]] + 1);
+                int minCandidate2 = Integer.MAX_VALUE;
+                if (coins[i] <= j) {
+                    minCandidate2 = dp[i][j - coins[i]] + 1;
                 }
+
+                int min = Math.min(minCandidate2, minCandidate1);
+                dp[i][j] = min != Integer.MAX_VALUE ? min : 0;
             }
         }
-        return dp[coins.length - 1][sum] == Integer.MAX_VALUE ? 0 : dp[coins.length - 1][sum];
+        return dp[coins.length - 1][sum];
     }
 }
--- src/test/java/dp/unlimited/MinimumCoinChangeTabularTest.java
+++ src/test/java/dp/unlimited/MinimumCoinChangeTabularTest.java
@@ -10,7 +10,7 @@ class MinimumCoinChangeTabularTest {
     public void shouldReturn2() {
         int[] coins = new int[]{1,2,3};
         int amount = 5;
-        MinimumCoinChangeTabular minimumCoinChangeTabular = new MinimumCoinChangeTabular();
+        MinimumCoinChangeTabular2 minimumCoinChangeTabular = new MinimumCoinChangeTabular2();
         assertEquals(2, minimumCoinChangeTabular.minimumNumberOfCoins(coins, amount));
     }
 
@@ -19,7 +19,7 @@ class MinimumCoinChangeTabularTest {
     public void shouldReturn4() {
         int[] coins = new int[]{1, 2, 3};
         int amount = 11;
-        MinimumCoinChangeTabular minimumCoinChangeTabular = new MinimumCoinChangeTabular();
+        MinimumCoinChangeTabular2 minimumCoinChangeTabular = new MinimumCoinChangeTabular2();
         assertEquals(4, minimumCoinChangeTabular.minimumNumberOfCoins(coins, amount));
     }
 
